1fc443bf49d32b68045ea9b943f9038f76ae299b
Nicolai-Desktop <s164438@student.dtu.dk>
2020-06-26 10:29:09 +0200

    #12 Removed unused files

--- compile_sally_anne.sh
+++
@@ -1,4 +0,0 @@
-dot -Tpng Sally_Anne0.dot > Sally_Anne0.png
-dot -Tpng Sally_Anne1.dot > Sally_Anne1.png
-dot -Tpng Sally_Anne2.dot > Sally_Anne2.png
-dot -Tpng Sally_Anne3.dot > Sally_Anne3.png
--- compile_states.sh
+++
@@ -1,2 +0,0 @@
-dot -Tpng State_Before.dot > State_Before.png
-dot -Tpng State_After.dot > State_After.png
