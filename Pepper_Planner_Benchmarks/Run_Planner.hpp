9b8b2b94aaee755dc50df50be2f952a69d01840a
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 11:48:05 +0200

    Run_Planner added namespace

--- Pepper_Planner_Benchmarks/Run_Planner.hpp
+++ Pepper_Planner_Benchmarks/Run_Planner.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include "DEL_Interface.hpp"
 
 #include <string>
-
-void find_and_execute(std::string file_name, const std::string& planning_agent);
+namespace del {
+	void find_and_execute(std::string file_name, const std::string& planning_agent);
+	void execute(DEL_Interface& del_interface);
+}
