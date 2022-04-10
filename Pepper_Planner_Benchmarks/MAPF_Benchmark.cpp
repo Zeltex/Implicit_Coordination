790d8435902e8f02383592b3b6350a4c9fafb9c0
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 11:37:50 +0200

    Added benchmark run and solve method

--- Pepper_Planner_Benchmarks/MAPF_Benchmark.cpp
+++ Pepper_Planner_Benchmarks/MAPF_Benchmark.cpp
@@ -3,38 +3,57 @@
 #include "DEL_Interface.hpp"
 
 
-using namespace del;
-void run_mapf_benchmark() {
-	std::string planning_agent = "a0";
-	std::vector<long> times;
+namespace del {
+	void run_mapf_benchmark() {
+		std::string planning_agent = "a0";
+		std::vector<long> times;
 
-	//std::string file_name = "../Examples/MAPF/p4.maepl";
-	//DEL_Interface del_interface(file_name);
-	//auto time_start = std::chrono::high_resolution_clock::now();
-	//del_interface.create_policy(planning_agent);
-	//auto time_end = std::chrono::high_resolution_clock::now();
+		for (size_t i = 13; i <= 15; ++i) {
+			std::string file_name = "../Examples/MAPF/p" + std::to_string(i) + ".maepl";
+			DEL_Interface del_interface(file_name);
+			auto time_start = std::chrono::high_resolution_clock::now();
+			del_interface.create_policy(planning_agent, true);
+			auto time_end = std::chrono::high_resolution_clock::now();
 
-	//times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
-	//std::cout << "P4 took ms " << times.back() << std::endl;
+			times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
+			std::cout << "P" << i << " took ms " << times.back() << "\n\n";
 
-	for (size_t i = 13; i <= 15; ++i) {
-		std::string file_name = "../Examples/MAPF/p" + std::to_string(i) + ".maepl";
-		DEL_Interface del_interface(file_name);
-		auto time_start = std::chrono::high_resolution_clock::now();
-		del_interface.create_policy(planning_agent, true);
-		auto time_end = std::chrono::high_resolution_clock::now();
+			std::ofstream output;
+			output.open("../Benchmarks/Benchmarks_mapfdu.csv");
+			for (size_t j = 0; j < times.size(); ++j) {
+				output << "P" << (j + 1) << ": " << times[j] << " ms\n";
+			}
+			output.close();
 
-		times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
-		std::cout << "P" << i << " took ms " << times.back() << "\n\n";
-
-		std::ofstream output;
-		output.open("../Benchmarks/Benchmarks_mapfdu.csv");
-		for (size_t j = 0; j < times.size(); ++j) {
-			output << "P" << (j + 1) << ": " << times[j] << " ms\n";
 		}
-		output.close();
 
+		__debugbreak;
 	}
+	void run_mapf_and_solve() {
+		std::string planning_agent = "a0";
+		std::vector<long> times;
+		for (size_t i = 1; i <= 15; ++i) {
+			std::string file_name = "../Examples/MAPF/p" + std::to_string(i) + ".maepl";
+			DEL_Interface del_interface(file_name);
+			auto time_start = std::chrono::high_resolution_clock::now();
+			del_interface.create_policy(planning_agent, true);
+			del_interface.create_policy(planning_agent, false);
+			auto time_end = std::chrono::high_resolution_clock::now();
 
-	__debugbreak;
+			times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
+			std::cout << "P" << i << " took ms " << times.back() << "\n\n";
+
+			execute(del_interface);
+
+			std::ofstream output;
+			output.open("../Benchmarks/Benchmarks_mapfdu.csv");
+			for (size_t j = 0; j < times.size(); ++j) {
+				output << "P" << (j + 1) << ": " << times[j] << " ms\n";
+			}
+			output.close();
+
+		}
+
+		__debugbreak;
+	}
 }
