f3e9d18ba8a6dce52c5b6e610764db0430d9590e
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 13:23:59 +0200

    Added MAPF_Benchmark folder argument

--- Pepper_Planner_Benchmarks/MAPF_Benchmark.cpp
+++ Pepper_Planner_Benchmarks/MAPF_Benchmark.cpp
@@ -4,34 +4,34 @@
 
 
 namespace del {
-	void run_mapf_benchmark() {
+	void run_mapf_benchmark(const std::string& folder) {
 		std::string planning_agent = "a0";
 		std::vector<long> times;
 
 		for (size_t i = 13; i <= 15; ++i) {
-			std::string file_name = "../Examples/MAPF/p" + std::to_string(i) + ".maepl";
+			std::string file_name = folder + "p" + std::to_string(i) + ".maepl";
 			DEL_Interface del_interface(file_name);
 			auto time_start = std::chrono::high_resolution_clock::now();
 			del_interface.create_policy(planning_agent, true);
 			auto time_end = std::chrono::high_resolution_clock::now();
 
 			times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
-			std::cout << "P" << i << " took ms " << times.back() << "\n\n";
+			std::cout << "P" << i << " took ms " << times.back() << std::endl << std::endl;
 
 			std::ofstream output;
 			output.open("../Benchmarks/Benchmarks_mapfdu.csv");
 			for (size_t j = 0; j < times.size(); ++j) {
-				output << "P" << (j + 1) << ": " << times[j] << " ms\n";
+				output << "P" << (j + 1) << ": " << times[j] << " ms" << std::endl;
 			}
 			output.close();
 
 		}
 	}
-	void run_mapf_and_solve() {
+	void run_mapf_and_solve(const std::string& folder) {
 		std::string planning_agent = "a0";
 		std::vector<long> times;
 		for (size_t i = 1; i <= 15; ++i) {
-			std::string file_name = "../Examples/MAPF/p" + std::to_string(i) + ".maepl";
+			std::string file_name = folder + "p" + std::to_string(i) + ".maepl";
 			DEL_Interface del_interface(file_name);
 			auto time_start = std::chrono::high_resolution_clock::now();
 			del_interface.create_policy(planning_agent, true);
@@ -39,14 +39,14 @@ namespace del {
 			auto time_end = std::chrono::high_resolution_clock::now();
 
 			times.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count());
-			std::cout << "P" << i << " took ms " << times.back() << "\n\n";
+			std::cout << "P" << i << " took ms " << times.back() << std::endl << std::endl;
 
 			execute(del_interface);
 
 			std::ofstream output;
 			output.open("../Benchmarks/Benchmarks_mapfdu.csv");
 			for (size_t j = 0; j < times.size(); ++j) {
-				output << "P" << (j + 1) << ": " << times[j] << " ms\n";
+				output << "P" << (j + 1) << ": " << times[j] << " ms" << std::endl;
 			}
 			output.close();
 
