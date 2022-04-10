ea919444f96f65c87e9c42dfcbd41c66c2f69943
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 14:07:09 +0200

    Benchmark find and execute split

--- Pepper_Planner_Benchmarks/Run_Planner.cpp
+++ Pepper_Planner_Benchmarks/Run_Planner.cpp
@@ -42,22 +42,34 @@ namespace del {
 		}
 	}
 
-	void find_and_execute(std::string file_name, const std::string& planning_agent) {
+	DEL_Interface find_plan2(const std::string& file_name, const std::string& planning_agent) {
+		return DEL_Interface("../Examples/" + file_name);
+	}
+
+	DEL_Interface find_plan(const std::string& file_name, const std::string& planning_agent) {
 		auto time1 = std::chrono::high_resolution_clock::now();
 
 		DEL_Interface del_interface("../Examples/" + file_name);
 
 		auto time2 = std::chrono::high_resolution_clock::now();
+		auto elapsed1 = time2 - time1;
+		std::cout << ".maepl load time " << (elapsed1.count() / 1000000) << "ms\n";
 		del_interface.create_policy(planning_agent);
 		auto time3 = std::chrono::high_resolution_clock::now();
 
-		auto elapsed1 = time2 - time1;
+		//auto elapsed1 = time2 - time1;
 		auto elapsed2 = time3 - time2;
 
 
 
-		std::cout << ".maepl load time " << (elapsed1.count() / 1000000) << "ms\n";
+		//std::cout << ".maepl load time " << (elapsed1.count() / 1000000) << "ms\n";
 		std::cout << "planning execution time " << (elapsed2.count() / 1000000) << "ms\n";
+		return del_interface;
+	}
+
+
+	void find_and_execute(std::string file_name, const std::string& planning_agent) {
+		auto del_interface = find_plan(file_name, planning_agent);
 
 		execute(del_interface);
 	}
