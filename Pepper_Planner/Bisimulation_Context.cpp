1c9a5e4c7a8fac11d550e7fa143e03cc638f6390
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 13:46:02 +0200

    Bisim_Context valuation fix

--- Pepper_Planner/Bisimulation_Context.cpp
+++ Pepper_Planner/Bisimulation_Context.cpp
@@ -1,12 +1,14 @@
 #include "Bisimulation_Context.hpp"
+#include "Core.hpp"
 #include <set>
+#include <map>
 
 namespace del {
 
 #if OPR_ENABLED == 1
 	class Valuations {
 	public:
-		void add(const std::string& valuation) { signatures.insert(signature); }
+		void add(const std::string& valuation) { valuations.insert(valuation); }
 		const std::set<std::string>& get() { return valuations; }
 		const size_t size() const { return valuations.size(); }
 	private:
