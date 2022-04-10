9977d32b7f95c8720fafbc885d552895cea14c22
Nicolai Herrmann <nicolai.herrmann.dk@gmail.com>
2022-04-10 13:28:03 +0200

    CMake options update

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 MODE=${1:-Release}
 mkdir -p Build
 cd Build
-cmake .. -DCMAKE_BUILD_TYPE=$MODE
+cmake .. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_CXX_COMPILER=g++
 cmake --build . --config $MODE
 ./$MODE/Pepper_Planner "../Examples/MAPF/"
